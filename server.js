20f13c91e77fef0c9a84d36fd531dd3609f535a1
Ewa Czech <ewa.czech@hotmail.com>
2020-05-09 19:27:43 +0200

    add error 500

--- server.js
+++ server.js
@@ -20,20 +20,24 @@ let users = [
 
 server.post('/api/users', (req, res) => {
 	const userInfo = req.body;
-	if (!userInfo.name || !userInfo.bio) {
-		res
-			.status(400)
-			.json({ errorMessage: 'Please provide name and bio for the user.' });
-	} else {
-		userInfo.id = shortid.generate();
-		users.push(userInfo);
-		if (res.status === 500) {
-			res.status(500).json({
-				errorMessage: 'There was an error while saving the user to the database.',
-			});
+	try {
+		if (!userInfo.name || !userInfo.bio) {
+			res
+				.status(400)
+				.json({ errorMessage: 'Please provide name and bio for the user.' });
 		} else {
-			res.status(201).json(userInfo);
+			userInfo.id = shortid.generate();
+			users.push(userInfo);
+			if (res.status === 500) {
+				res.status(500).json({
+					errorMessage: 'There was an error while saving the user to the database.',
+				});
+			} else {
+				res.status(201).json(userInfo);
+			}
 		}
+	} catch {
+		res.status(500).json({errorMessage: "There was an error while saving the user to the database"});
 	}
 });
 
@@ -42,13 +46,16 @@ server.post('/api/users', (req, res) => {
 //----------------------------------------------------------------------------------------------------------------
 
 server.get('/api/users', (req, res) => {
-	if (users) {
-		res.status(200).json(users);
-	} else {
-		res
-			.status(500)
-			.json({ errorMessage: 'The users information could not be retrieved.' });
-	}
+	
+		if (users) {
+			res.status(200).json(users);
+		} else {
+			res
+				.status(500)
+				.json({ errorMessage: 'The users information could not be retrieved.' });
+		}
+	
+	
 });
 
 //----------------------------------------------------------------------------------------------------------------
@@ -82,15 +89,20 @@ server.delete('/api/users/:id', (req, res) => {
 	const found = users.find((user) => {
 		return user.id == id;
 	});
-
-	if (found) {
-		users = users.filter((user) => user.id !== id);
-		res.status(200).json(found);
-	} else {
-		res
-			.status(404)
-			.json({ message: 'The user with the specified ID does not exist.' });
+	try {
+		if (found) {
+			users = users.filter((user) => user.id !== id);
+			res.status(200).json(found);
+		} else {
+			res
+				.status(404)
+				.json({ message: 'The user with the specified ID does not exist.' });
+		}
 	}
+	catch {
+		res.status(500).json({errorMessage: "The user could not be removed"})
+	}
+	
 });
 
 //----------------------------------------------------------------------------------------------------------------
@@ -101,26 +113,31 @@ server.put('/api/users/:id', (req, res) => {
 	const { id } = req.params;
 
 	const changes = req.body;
-
-	if (!id) {
-		res
-			.status(404)
-			.json({ message: 'The user with the specified ID does not exist.' });
-	}
-	if (!changes.bio || !changes.name) {
-		res
-			.status(400)
-			.json({ errorMessage: 'Please provide name and bio for the user.' });
-	}
-
-	users = users.map((user) => {
-		if (user.id === id) {
-			return changes;
-		} else {
-			return user;
+	try {
+		if (!id) {
+			res
+				.status(404)
+				.json({ message: 'The user with the specified ID does not exist.' });
 		}
-	});
-	res.status(200).json(req.body);
+		if (!changes.bio || !changes.name) {
+			res
+				.status(400)
+				.json({ errorMessage: 'Please provide name and bio for the user.' });
+		}
+	
+		users = users.map((user) => {
+			if (user.id === id) {
+				return changes;
+			} else {
+				return user;
+			}
+		});
+		res.status(200).json(req.body);
+	}
+	catch {
+		res.status(500).json({error:"erro"});
+	}
+	
 });
 
 server.get('/', function (req, res) {
